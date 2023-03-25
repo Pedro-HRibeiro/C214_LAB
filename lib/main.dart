b22a86f776ea637f97eb731aea49ad1761b04803
Pedro-HRibeiro <pedrohenrique.ph@gec.inatel.br>
2023-03-24 22:50:24 -0300

    criado botao principal

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,97 @@ class _MyAppState extends State<MyApp> {
     futurePhotos = fetchPhotos();
   }
 
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Arquivos das Fotos')),
+        body: const Center(
+          child: DialogExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
   @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Nome do usuário'),
+          content: TextEx(),
+          
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Clique para informações'),
+    );
+  }
+}
+
+class TextEx extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Arquivos das Fotos',
+      theme: ThemeData(
+        primarySwatch: Colors.purple
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Arquivos das Fotos:'),
+        ),
+        body: Center(
+          child: FutureBuilder<Photos>(
+            future: fetchPhotos(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Text(snapshot.data!.albumId.toString()),
+                    Text(snapshot.data!.id.toString()),
+                    Text(snapshot.data!.titulo),
+                    Text(snapshot.data!.url),
+                    Text(snapshot.data!.thumbnailUrl)
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Arquivos das Fotos',
@@ -93,4 +183,5 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-}
+}*/
+
